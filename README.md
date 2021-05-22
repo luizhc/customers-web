0cc10e80b90f2a23fab7f7debda288efe569ac91
Luiz Henrique <luizhenriquecorreia1997@gmail.com>
2021-05-22 14:45:47 -0300

    feat: navbar

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CustomersWeb
+# Customers
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.1.0.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
 import { HomeComponent } from './features/home/home.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
