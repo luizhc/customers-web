f57ff10052a86ffeaf80f80ac007298e40b36956
Luiz Henrique <luizhenriquecorreia1997@gmail.com>
2021-05-22 15:24:53 -0300

    feat: footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { HomeComponent } from './features/home/home.component';
 
@@ -9,7 +10,8 @@ import { HomeComponent } from './features/home/home.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
