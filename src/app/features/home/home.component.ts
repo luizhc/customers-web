7a89682907334a7f80c0ee808fdf13254327b0d9
Luiz Henrique <luizhenriquecorreia1997@gmail.com>
2022-03-03 22:12:19 -0300

    refactor: fix path import

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UtilsService } from 'src/app/services/utils.service';
+import { UtilsService } from '../../services/utils.service';
 
 @Component({
   selector: 'app-home',
